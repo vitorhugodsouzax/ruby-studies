6fa5fe804e8b3826b6c4c02db273239385757f7f
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-05-15 17:44:42 -0300

    Fiz uma simples calculadora

--- Introduction/Strings2.rb
+++ Introduction/Strings2.rb
@@ -14,3 +14,14 @@ total_characters = first_name.length + last_name.length
 puts "My name in reversed is #{first_name} #{last_name}"
 puts "My name in reversed is #{reversed_first_name} #{reversed_last_name}"
 puts "My total characters is: #{total_characters}"
+
+#posso fazer dessa forma tambem
+
+# puts "Enter your first name"
+# first_name = gets.chomp
+# puts "Enter your last name"
+# last_name = gets.chomp
+# full_name = first_name + " " + last_name
+# puts "Your full name is #{full_name}"
+# puts "Your full name reversed is #{full_name.reverse}"
+# puts "Your name has #{full_name.length - 1} characters in it"
