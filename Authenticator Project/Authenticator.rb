9aae5d76a1bd7fa21344ce682c18e89c2ff215d1
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-05-22 17:54:47 -0300

    Authenticator ready

--- Authenticator Project/Authenticator.rb
+++ Authenticator Project/Authenticator.rb
@@ -1,3 +1,4 @@
+# users array where username and password are stored
 users = [
 { username: "vitor", password: "password1"},
 { username: "joao", password: "password2"},
@@ -6,15 +7,25 @@ users = [
 { username: "kaua", password: "password5"}
 ]
 
+# authentication method to check and verify if username/password combination exists
 
+def auth_user (username, password, list_of_users)
+   list_of_users.each do |user_record|
+        if username == user_record[:username] && password == user_record[:password]
+            return user_record
+        end
+    end
+    "Credentials were not correct"
+end
+
+# program execution flow
+ 
 puts "welcome to the authenticator"
 25.times{ print "-"}
 puts
 puts "This program will take input from the user and compare password"
 puts "If password is correct, you will get back the user object"
 
-#we will learn
-# hash, array, branching, while loops and designing program execution flow
 
 
 attempts = 1
@@ -23,24 +34,16 @@ while attempts < 4
     username = gets.chomp
     print "Password: "
     password = gets.chomp
-
-    users.each do |user|
-        if username == user[:username] && password == user[:password]
-            puts user
-            break
-        else
-            puts "invalid username or password"
-        end
-    end
-
+    authentication = auth_user(username, password, users)
+    puts authentication
     puts "Press n to quit or any other key to continue: "
     input = gets.chomp.downcase
     break if input == "n"
     attempts += 1
 end
     
-  if attempts >= 4 
-    puts "authenticator is interrupted"
+  if attempts == 4
+    puts "You have exceeded the number of atempts"
   end
 
 
