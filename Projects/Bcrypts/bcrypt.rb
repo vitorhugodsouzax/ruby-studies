2819638d8b412ae859faade8e52a4843304040ac
vitor hugo <vitorhugodesouzaph@gmail.com>
2024-05-28 17:44:42 -0300

    project working

--- Projects/Bcrypts/bcrypt.rb
+++ Projects/Bcrypts/bcrypt.rb
@@ -3,6 +3,27 @@ require 'bcrypt'
 # my_password = BCrypt::Password.create("my password")
 # puts my_password
 
-my_password = BCrypt::Password.new("$2a$12$5f/LrZoQ4QEHIsGNfBgwhuPzB.fpcw9xieBfAlaQZ2ybqxJN8OjW6")
-puts  my_password == "my password"     #=> true
-# my_password == "not my password" #=> false
+users = [
+          { username: "vitor", password: "password1" },
+          { username: "joao", password: "password2" },
+          { username: "jose", password: "password3" },
+          { username: "kaua", password: "password4" },
+          { username: "lucas", password: "password5" }
+        ]
+ 
+def create_hash_digest(password)
+  BCrypt::Password.create(password)
+end
+ 
+def verify_hash_digest(password)
+  BCrypt::Password.new(password)
+end
+ 
+def create_secure_users(list_of_users)
+  list_of_users.each do |user_record|
+    user_record[:password] = create_hash_digest(user_record[:password])
+  end
+  list_of_users
+end
+
+puts create_secure_users(users)
