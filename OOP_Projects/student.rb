a4ddc8dc643fd2779acf690dd84b0941e41116ae
vitor hugo <vitorhugodesouzaph@gmail.com>
2024-05-31 17:38:12 -0300

    fixing

--- OOP_Projects/student.rb
+++ OOP_Projects/student.rb
@@ -1,60 +1,29 @@
-# class  Student
-     
-#     attr_accessor :first_name, :last_name, :email, :username
-#     attr_reader  
-
-#     @first_name
-#     @last_name
-#     @email
-#     @username
-#     @password
-
-# end
-
-# def to_s 
-# "First name: #{first_name}"
-# "Last name: #{last_name}"
-# "Email: #{email}"
-# "username: #{username}"
-# end
-
-# vitor = Student.new
-# puts "whats your first name?: "
-# vitor.first_name = gets.chomp
-# puts "whats you last name?: "
-# vitor.last_name = gets.chomp
-# vitor.email = "vitor@example.com"
-# vitor.username = "vitorhs"
-
-# puts "username is: #{vitor.first_name}"
-# puts "last name is: #{vitor.last_name}"
-# puts "email is: #{vitor.email}"
-# puts "username is: #{vitor.username}"
-
+require_relative 'crud'
+ 
 class Student
-    attr_accessor :first_name, :last_name, :email, :username, :password
-
-    def initialize(firstname, lastname, username, email, password)
-      @first_name = firstname
-      @last_name = lastname
-      @username = username
-      @email = email
-      @password = password
-    end
-   
-    def to_s
-      "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username},
-                    email address: #{@email}"
-    end
-   
+  include Crud
+  attr_accessor :first_name, :last_name, :email, :username, :password
+ 
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
   end
-   
-  mashrur = Student.new("Mashrur", "Hossain", "mashrur1", "mashrur@example.com",
-                        "password1")
-  john = Student.new("John", "Doe", "john1", "john1@example.com",
-                        "password2")
-  puts mashrur
-  puts john
-  mashrur.last_name = john.last_name
-  puts "Mashrur is altered"
-  puts mashrur
+ 
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username},
+                  email address: #{@email}"
+  end
+ 
+end
+ 
+vitor = Student.new("Vitor", "Hugo", "hugo1", "hugo@example.com",
+                      "password1")
+john = Student.new("John", "Doe", "john1", "john1@example.com",
+                      "password2")
+ 
+hashed_password = vitor.create_hash_digest(vitor.password)
+ 
+puts hashed_password
